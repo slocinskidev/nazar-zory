d385a46cc8fec450842e55fc32177ca4f92e3637
Eryk Slocinski <eryk.slocinski@gmail.com>
2021-01-15 00:48:33 +0100

    scroll icon now is animate

--- src/components/MouseScroll/MouseScroll.js
+++ src/components/MouseScroll/MouseScroll.js
@@ -1,5 +1,27 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const scroller = keyframes`
+	0% {
+		transform: translateY(0);
+	}
+
+  49% {
+    transform: translateY(200%);
+
+	}
+
+  95% {
+    transform: translateY(200%);
+
+	}
+
+	100% {
+    transform: translateY(0%);
+
+	}
+
+`;
 
 const OutlineWrapper = styled.div`
   width: 24px;
@@ -18,6 +40,7 @@ const Circle = styled.div`
   height: 12px;
   background-color: ${({ theme }) => theme.color.white};
   border-radius: 50%;
+  animation: ${scroller} 4s infinite 1s;
 `;
 
 const MouseScroll = () => {
